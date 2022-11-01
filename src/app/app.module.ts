73a2462ebf08fcb251c84cf70e5571606f9871d8
Daniel Mateo Patarroyo Molina <danielmateopatarroyomolina@gmail.com>
2022-11-01 16:34:46 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppComponent } from './app.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { HeaderComponent } from './core/header/header.component';
+import { HomePageComponent } from './modules/home/page/home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomePageComponent,
   ],
   imports: [
     BrowserModule
